fb2fa22516891584473d5eb95f86d74ba14459b9
MariaReznikova <reznikovam13@gmail.com>
2019-07-09 12:10:25 +0300

    Made footer and carousel.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+ 
+        navigation : true, // Show next and prev buttons
+   
+        slideSpeed : 300,
+        paginationSpeed : 400,
+   
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+   
+    });
+   
+  });
